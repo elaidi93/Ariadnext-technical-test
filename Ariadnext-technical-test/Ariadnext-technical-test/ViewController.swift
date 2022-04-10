41a795e9327ea32300279ea3b8297694f019e1d5
hamza <helaidi@exomind.fr>
2022-04-10 15:10:57 +0200

    add xcode project

---
+++ Ariadnext-technical-test/Ariadnext-technical-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ariadnext-technical-test
+//
+//  Created by Hamza EL Aidi on 10/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
