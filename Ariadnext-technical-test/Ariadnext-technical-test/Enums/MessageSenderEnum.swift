77c182bc2e6c7817979fba99fdaf7d8466f93023
hamza <helaidi@exomind.fr>
2022-04-10 19:32:15 +0200

    Change Sender enum name

---
+++ Ariadnext-technical-test/Ariadnext-technical-test/Enums/MessageSenderEnum.swift
@@ -0,0 +1,13 @@
+//
+//  MessageSenderEnum.swift
+//  Ariadnext-technical-test
+//
+//  Created by Hamza EL Aidi on 10/04/2022.
+//
+
+import Foundation
+
+enum Sender: CaseIterable {
+	case server
+	case client
+}
