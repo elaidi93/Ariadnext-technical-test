367d4352babb9743299dae6f9c8ba7d1637e334b
hamza <helaidi@exomind.fr>
2022-04-10 17:34:47 +0200

    Implement simple UI

--- Ariadnext-technical-test/Ariadnext-technical-test/Modules/Home/HomeViewController.swift
+++ Ariadnext-technical-test/Ariadnext-technical-test/Modules/Home/HomeViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+	
+	@IBOutlet private weak var tableView: UITableView! {
+		didSet {
+			tableView.delegate = self
+			tableView.dataSource = self
+		}
+	}
+	
+	@IBOutlet private weak var inputBottom: NSLayoutConstraint!
+	
+	private var messages = [String]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		print("home")
+		registerKeyboardNotifications()
 	}
 
+	func registerKeyboardNotifications() {
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(keyboardWillShow(_:)),
+											   name: UIResponder.keyboardWillShowNotification,
+											   object: nil)
+	 
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(keyboardWillHide(_:)),
+											   name: UIResponder.keyboardWillHideNotification,
+											   object: nil)
+	}
+	
+	@objc
+	func keyboardWillShow(_ sender: NSNotification) {
+		let info = sender.userInfo!
+		let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
 
+		inputBottom.constant = keyboardSize
+		tableView.scrollToRow(at: IndexPath(row: 19, section: 0), at: .bottom, animated: true)
+	}
+
+	@objc
+	func keyboardWillHide(_ sender: NSNotification) {
+		inputBottom.constant = 0
+		tableView.scrollToRow(at: IndexPath(row: 19, section: 0), at: .bottom, animated: true)
+	}
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return messages.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+}
