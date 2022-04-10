b7735440d61a4af8622def4f06d964c976a6eb85
hamza <helaidi@exomind.fr>
2022-04-10 22:01:24 +0200

    Add random responses enum

---
+++ Ariadnext-technical-test/Ariadnext-technical-test/Enums/ServerResponseEnum.swift
@@ -0,0 +1,20 @@
+//
+//  ServerResponseEnum.swift
+//  Ariadnext-technical-test
+//
+//  Created by Hamza EL Aidi on 10/04/2022.
+//
+
+import Foundation
+
+enum ServerResponse: String, CaseIterable {
+	
+	case bonjour = "Hello, How can i help you :)"
+	case response1 = "Please check your internet"
+	case response2 = "i need more infos"
+	case response3 = "Please restart your machine, and chek again"
+	case response4 = "if the probleme has not resolved, please contact us"
+	case response5 = "it coast 350€"
+	case response6 = "here is what i found in internet"
+	case response7 = "Do you have any other question ?"
+}
